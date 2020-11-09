5427dd68b90b1aa8f27bef4fbb32c50dc4b83d2b
Shusty <shujitsu@gmail.com>
2020-11-09 23:02:10 +0100

    Updates

---
+++ precache-manifest.6ac947d21234edcea3c342e72c03341f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a096bbaaf5908398ff9fc54654c74e7b",
+    "url": "/netcore-api-and-client-demo/index.html"
+  },
+  {
+    "revision": "a9bcbacbc549c6c91883",
+    "url": "/netcore-api-and-client-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d410db4a2ed5fe4e72d3",
+    "url": "/netcore-api-and-client-demo/static/js/2.3e826af4.chunk.js"
+  },
+  {
+    "revision": "fd93936ad05fb41a88e3492e1b7cea4b",
+    "url": "/netcore-api-and-client-demo/static/js/2.3e826af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bcbacbc549c6c91883",
+    "url": "/netcore-api-and-client-demo/static/js/main.e77c8c15.chunk.js"
+  },
+  {
+    "revision": "a8b176ae4e345f8f7689",
+    "url": "/netcore-api-and-client-demo/static/js/runtime-main.f877470d.js"
+  }
+]);
